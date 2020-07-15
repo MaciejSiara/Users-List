e7690627334d4396d20f7f6e4cb21f8dd4094ecb
Maciej Siara <siaramaciek1@gmail.com>
2020-07-15 18:19:21 +0200

    Updates

---
+++ precache-manifest.86820aac32dc3436098baebf993cf9b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c79f2f594d60d9652cca50c63478a0e1",
+    "url": "/MaciejSiara/Users-List/index.html"
+  },
+  {
+    "revision": "17070a69d0e25db13a6e",
+    "url": "/MaciejSiara/Users-List/static/css/main.5af081b9.chunk.css"
+  },
+  {
+    "revision": "71422366ca1b022b78d5",
+    "url": "/MaciejSiara/Users-List/static/js/2.5acd79ff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MaciejSiara/Users-List/static/js/2.5acd79ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17070a69d0e25db13a6e",
+    "url": "/MaciejSiara/Users-List/static/js/main.719a0cce.chunk.js"
+  },
+  {
+    "revision": "d6209d303a82ab987cce",
+    "url": "/MaciejSiara/Users-List/static/js/runtime-main.d14472ec.js"
+  }
+]);
